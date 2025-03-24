4dfe4053901bafe4e1728c58c66eaee3102b87b6
HeenTine <thaihuynhtai1207@gmail.com>
2025-03-24 16:30:34 +0700

    24/3

---
+++ Ex6_IntentDonGian/app/src/main/java/tiil/edu/ex6_intentdongian/MainActivity.java
@@ -0,0 +1,55 @@
+package tiil.edu.ex6_intentdongian;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button nutMH2,nutMH3;
+    void TimDieuKhien(){
+        nutMH2 = (Button) findViewById(R.id.btnMH2);
+        nutMH3 = (Button) findViewById(R.id.btnMH3);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //Tìm điều khiển nút bấm
+        TimDieuKhien();
+        //Gắn bộ lắng nghe
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Xử lý chuyển màn hình
+                //B1. Tạo một Intent 2 tham số : 1)Màn hình hiện tại.this; 2) Màn hình chuyển tới.class
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+                //B2. Gửi
+                startActivity(intentMH2);
+            }
+        });
+
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Xử lý chuyển màn hình
+                //B1. Tạo một Intent 2 tham số : 1)Màn hình hiện tại.this; 2) Màn hình chuyển tới.class
+                Intent intentMH3 = new Intent(MainActivity.this, MH3Activity.class);
+                //B2. Gửi
+                startActivity(intentMH3);
+            }
+        });
+    }
+}
