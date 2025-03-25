35351742365c22679b1b7436f78db109199e6eae
HeenTine <thaihuynhtai1207@gmail.com>
2025-03-25 19:30:49 +0700

    25/3

---
+++ Ex6_IntentDonGian_LamLai/app/src/main/java/tiil/edu/myapplication/MH3Activity.java
@@ -0,0 +1,39 @@
+package tiil.edu.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MH3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_mh3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TimDieuKhien();
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentBack = new Intent(MH3Activity.this, MainActivity.class);
+                startActivity(intentBack);
+            }
+        });
+    }
+    Button btnBack;
+    void TimDieuKhien(){
+        btnBack = (Button) findViewById(R.id.btnBack);
+    }
+}
